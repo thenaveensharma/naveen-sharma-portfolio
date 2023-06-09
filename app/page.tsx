ccf50c6ab2a94e74f74f7261b1df135cc36e1e5e
Naveen Kumar <naveenchaurasiya1501@gmail.com>
2023-06-09 09:51:28 +0530

    about section  implemented

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,24 @@
+import About from "@/components/About";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Head from "next/head";
-import Image from "next/image";
 
 export default function Home() {
   return (
     // className="flex min-h-screen flex-col items-center justify-between p-24"
 
-    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0 ">
       <Head>
         <title>The Naveen Sharma</title>
       </Head>
 
       <Header />
-
-      {/* Hero */}
-      <section id="hero" className="snap-center">
+      <section id="hero" className="snap-start">
         <Hero />
       </section>
-      {/* About */}
-
+      <section id="about" className="snap-center">
+        <About />
+      </section>
       {/* Experience */}
 
       {/* Skills */}
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,45 +3,56 @@ import React from "react";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 import BackgroundCircles from "./BackgroundCircles";
 import Image from "next/image";
+import Link from "next/link";
 
 type Props = {};
 
-export default function Hero({ }: Props) {
-    const [text, count] = useTypewriter({
-        words: [
-            "Hi I'm Naveen Sharma",
-            "Guy-who-loves-coffee.tsx",
-            "<ButLovesToCodeMore />",
-        ],
-        loop: true,
-        delaySpeed: 2000,
-    });
+export default function Hero({}: Props) {
+  const [text, count] = useTypewriter({
+    words: [
+      "Hi I'm Naveen Kumar",
+      "Guy-who-loves-Coffee.tsx",
+      "<ButLovesToCodeMore />",
+    ],
+    loop: true,
+    delaySpeed: 100,
+    typeSpeed: 100,
+    deleteSpeed: 100,
+  });
 
-    return (
-        <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
-            <BackgroundCircles />
-            <Image
-                src="/images/Logo.jpg"
-                alt=""
-                width={200}
-                height={200}
-                className="relative rounded-full h-32 w-32 mx-auto object-cover"
-            />
-            <div>
-                <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
-                    Software Engineer
-                </h2>
-                <h1 className="text-5xl lg:text-6xl font-semibold px-10">
-                    <span>{text}</span>
-                    <Cursor cursorColor="#F7AB0A" />
-                </h1>
-                <div>
-                    <button className="hero_button">About</button>
-                    <button className="hero_button">Experience</button>
-                    <button className="hero_button">Skills</button>
-                    <button className="hero_button">Projects</button>
-                </div>
-            </div>
+  return (
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+      <BackgroundCircles />
+      <Image
+        src="/images/Logo.jpg"
+        alt=""
+        width={200}
+        height={200}
+        className="relative rounded-full h-32 w-32 mx-auto object-cover"
+      />
+      <div className="z-20">
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+          Software Engineer
+        </h2>
+        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+          <span>{text}</span>
+          <Cursor cursorColor="#F7AB0A" />
+        </h1>
+        <div className="mt-5">
+          <Link href={"#about"}>
+            <button className="hero_button">About</button>
+          </Link>
+          <Link href={"#experience"}>
+            <button className="hero_button">Experience</button>
+          </Link>
+          <Link href={"#skills"}>
+            <button className="hero_button">Skills</button>
+          </Link>
+          <Link href={"#projects"}>
+            <button className="hero_button">Projects</button>
+          </Link>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
